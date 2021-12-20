988299ad70a0a123a0d9ab5080ac3a2e4d800ec1
Hbprado <56452678+Hbprado@users.noreply.github.com>
2021-12-20 01:53:43 -0300

    att

---
+++ SQL/Consultas.sql
@@ -0,0 +1,37 @@
+# Consultas
+use dunder_mifflin;
+
+# Consulta com GROUP BY/HAVING
+#Exibir funcionarios que venderam mais de 3 produtos
+SELECT id_funcionario, COUNT(*)AS qtd 
+FROM venda
+GROUP BY id_funcionario
+HAVING COUNT(*) > 3;
+#Subconsulta do tipo escalar
+#Todos os nomes dos funcionarios com idade maior que o gerente de id = '2'
+SELECT F.nome, F.idade
+FROM funcionario F
+WHERE F.idade < (
+    SELECT F1.idade
+    FROM Funcionario F1
+    WHERE F1.id_funcionario = 2
+); 
+#Subconsulta do tipo linha
+# Funcionarios com a mesma idade e que sao da mesma distribuidora que o funcionario "Heitor Prado" exceto ele mesmo
+SELECT F.nome
+FROM Funcionario F
+WHERE F.nome <> 'Heitor Prado' 
+AND (F.idade, F.id_distribuidora) = (
+    SELECT F1.idade, F1.id_distribuidora
+    FROM funcionario F1
+    WHERE F1.nome = 'Heitor Prado'
+); 
+#Juncao Interna
+#CPF dos clientes que compraram papeis 
+SELECT nome, V.cpf
+FROM produto P INNER JOIN venda V ON V.id_produto = P.id_produto
+WHERE nome = 'Papeis';
+
+
+
+
